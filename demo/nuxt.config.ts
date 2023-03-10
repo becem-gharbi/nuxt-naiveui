f37f8a41b6f817cc74b2739922dd0bbe031e506c
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-03-10 07:51:33 +0100

    chore: update demo app

--- demo/nuxt.config.ts
+++ demo/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt Naive UI",
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
   ssr: true,
   modules: ["@bg-dev/nuxt-naiveui"],
   css: ["~/assets/fonts/gellix/style.css"],
