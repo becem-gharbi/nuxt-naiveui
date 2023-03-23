938ee66f498095de9c63d1733d5902e3c5a1416c
becem-gharbi <99251251+becem-gharbi@users.noreply.github.com>
2023-03-23 13:14:51 +0100

    fix: add missing vite optimizations

--- src/module.ts
+++ src/module.ts
@@ -5,6 +5,7 @@ import {
   addImportsDir,
   addComponent,
   addImports,
+  extendViteConfig,
 } from "@nuxt/kit";
 import { fileURLToPath } from "url";
 import type { ThemeConfig, ColorModePreference } from "./runtime/types";
@@ -116,7 +117,7 @@ export default defineNuxtModule<ModuleOptions>({
       });
     });
 
-    // Transpile naive modules
+    // https://www.naiveui.com/en-US/os-theme/docs/ssr
     if (process.env.NODE_ENV === "production") {
       nuxt.options.build.transpile.push(
         "naive-ui",
@@ -126,6 +127,16 @@ export default defineNuxtModule<ModuleOptions>({
       );
     } else {
       nuxt.options.build.transpile.push("@juggle/resize-observer");
+
+      extendViteConfig((config) => {
+        config.optimizeDeps = config.optimizeDeps || {};
+        config.optimizeDeps.include = config.optimizeDeps.include || [];
+        config.optimizeDeps.include.push(
+          "naive-ui",
+          "vueuc",
+          "date-fns-tz/esm/formatInTimeZone"
+        );
+      });
     }
   },
 });
