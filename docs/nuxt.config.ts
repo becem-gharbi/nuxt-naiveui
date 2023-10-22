3c17701748d1dafef5beb74d7e5126f2bf09444d
becem-gharbi <becem.gharbi@live.com>
2023-10-22 11:42:04 +0100

    docs: Remove unused nuxt-naiveui module

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,12 +1,11 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: ["@nuxt-themes/docus"],
 
   modules: [
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
+    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools',
-    '@bg-dev/nuxt-naiveui'
-  ]
-})
+    "@nuxt/devtools",
+  ],
+});
