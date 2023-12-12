a98bc63ad93ec8a4507c254f20b20d61dcfca46c
becem-gharbi <becem.gharbi@live.com>
2023-12-12 17:16:46 +0100

    feat(loading bar): Add navigation prop to show on page loading (#13)

--- src/runtime/components/NaiveLoadingBar.client.vue
+++ src/runtime/components/NaiveLoadingBar.client.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script setup lang="ts">
+import { useNuxtApp } from "#imports"
 import { useLoadingBar  } from "naive-ui"
 
 const { error, finish, start } = useLoadingBar();
@@ -10,4 +11,11 @@ const { error, finish, start } = useLoadingBar();
 window.addEventListener("naiveui:loadingBar:start", start);
 window.addEventListener("naiveui:loadingBar:finish", finish);
 window.addEventListener("naiveui:loadingBar:error", error);
+
+const props = defineProps<{ navigation?: boolean }>()
+
+if (props.navigation) {
+  useNuxtApp().hook('page:start', start)
+  useNuxtApp().hook('page:finish', finish)
+}
 </script>
