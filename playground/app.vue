a8cd2c4ed180841b20d835e92ad567720b131904
becem-gharbi <becem.gharbi@live.com>
2023-12-14 18:46:23 +0100

    chore(playground): create default layout

--- playground/app.vue
+++ playground/app.vue
@@ -1,80 +1,19 @@
 <template>
   <NaiveConfig :theme-config="themeConfig">
-    <NaiveLayoutNavbar
-      :routes="navbarRoutes"
-      :drawer-routes="drawerRoutes"
-    >
-      <template #start>
-        <nuxt-link to="/">
-          <NaiveIcon name="logos:naiveui" />
-        </nuxt-link>
-      </template>
+    <client-only>
+      <n-loading-bar-provider>
+        <naive-loading-bar navigation />
+      </n-loading-bar-provider>
+    </client-only>
 
-      <template #end>
-        <NuxtLink
-          to="https://github.com/becem-gharbi/nuxt-naiveui"
-          target="_"
-          aria-label="source-code"
-        >
-          <n-button
-            text
-            tag="span"
-          >
-            <NaiveIcon name="cib:github" />
-          </n-button>
-        </NuxtLink>
-        <NaiveColorModeSwitch />
-      </template>
-
-      <template #drawer-header>
-        <n-text>Header</n-text>
-      </template>
-
-      <template #drawer-footer>
-        <n-text>Footer</n-text>
-      </template>
-
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-    </NaiveLayoutNavbar>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </NaiveConfig>
 </template>
 
 <script setup lang="ts">
-import type { ThemeConfig, NavbarRoute } from "../src/module";
-
-const navbarRoutes: NavbarRoute[] = [
-  {
-    label: "Sample, this is a long long lable",
-    icon: "ri:home-4-line",
-    path: "/",
-  },
-  {
-    label: "Computers",
-    icon: "material-symbols:computer-outline",
-    path: "/computers",
-  },
-  {
-    label: "Categories",
-    icon: "mdi:bookmark-multiple-outline",
-    children: [
-      {
-        label: "Books",
-        icon: "material-symbols:menu-book-outline-rounded",
-        path: "/books",
-      },
-    ],
-  },
-];
-
-const drawerRoutes: NavbarRoute[] = [
-  {
-    label: "Sample, this is a long long lable",
-    icon: "ri:home-4-line",
-    path: "/",
-  },
-];
+import type { ThemeConfig } from "../src/module";
 
 const themeConfig: ThemeConfig = {
   // shared: {
---
+++ playground/layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <NaiveLayoutSidebar
+    :routes="navbarRoutes"
+    :drawer-routes="drawerRoutes"
+  >
+    <template #start>
+      <nuxt-link to="/">
+        <NaiveIcon name="logos:naiveui" />
+      </nuxt-link>
+    </template>
+
+    <template #end>
+      <NuxtLink
+        to="https://github.com/becem-gharbi/nuxt-naiveui"
+        target="_"
+        aria-label="source-code"
+      >
+        <n-button
+          text
+          tag="span"
+        >
+          <NaiveIcon name="cib:github" />
+        </n-button>
+      </NuxtLink>
+      <NaiveColorModeSwitch />
+    </template>
+
+    <template #drawer-header>
+      <n-text>Header</n-text>
+    </template>
+
+    <template #drawer-footer>
+      <n-text>Footer</n-text>
+    </template>
+
+    <slot />
+  </NaiveLayoutSidebar>
+</template>
+
+<script setup lang="ts">
+import type { NavbarRoute } from "../../src/module";
+
+const navbarRoutes: NavbarRoute[] = [
+  {
+    label: "Sample, this is a long long lable",
+    icon: "ri:home-4-line",
+    path: "/",
+  },
+  {
+    label: "Computers",
+    icon: "material-symbols:computer-outline",
+    path: "/computers",
+  },
+  {
+    label: "Categories",
+    icon: "mdi:bookmark-multiple-outline",
+    children: [
+      {
+        label: "Books",
+        icon: "material-symbols:menu-book-outline-rounded",
+        path: "/books",
+      },
+    ],
+  },
+];
+
+const drawerRoutes: NavbarRoute[] = [
+  {
+    label: "Sample, this is a long long lable",
+    icon: "ri:home-4-line",
+    path: "/",
+  },
+];
+</script>
