fd869406a75d4c32ff51239b4e6331029982dbf4
becem-gharbi <becem.gharbi@live.com>
2024-01-25 09:22:26 +0100

    fix(NaiveNotification): only register a single listener

--- src/runtime/components/NaiveNotification.client.vue
+++ src/runtime/components/NaiveNotification.client.vue
@@ -5,15 +5,20 @@
 <script setup lang="ts">
 import { useNotification } from "naive-ui";
 import type { NotificationOptions } from "naive-ui";
+import { callOnce } from '#imports'
 
 interface CustomEvent extends Event {
   detail: NotificationOptions;
 }
 
-const { create } = useNotification();
+await callOnce('naive-notification-add-listener', () => {
+  const { create } = useNotification();
 
-window.addEventListener("naiveui:notification:create", (event) => {
-  const { detail } = event as CustomEvent;
-  create(detail);
-});
+  window.addEventListener("naiveui:notification:create", (event) => {
+    const { detail } = event as CustomEvent;
+    create(detail);
+  });
+
+  return true
+})
 </script>
