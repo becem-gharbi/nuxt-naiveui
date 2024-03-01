0caca9e27dc8221b1e8e6452e61632891402b959
becem-gharbi <becem.gharbi@live.com>
2024-03-01 12:31:26 +0100

    docs: change font-family

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,22 @@
 export default defineNuxtConfig({
   extends: ["@nuxt-themes/docus"],
+  imports: {
+    autoImport: true
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;600&display=swap'
+        }
+      ]
+    }
+  },
 });
