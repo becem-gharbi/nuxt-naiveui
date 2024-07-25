e46e4438cd03e899953f2925068e4747a80e9e09
becem-gharbi <becem.gharbi@live.com>
2024-07-25 08:24:30 +0100

    test: add `icon` test suite

---
+++ playground/app/pages/icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <naive-icon :name />
+    <button
+      data-testid="icon-name-toggle"
+      @click="name = 'ph:hand'"
+    >
+      Toggle
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from '#imports'
+
+const name = ref('ph:bird')
+</script>
