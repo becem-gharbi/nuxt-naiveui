684ff77bce46cdf76d9d3048199564de87b03386
becem-gharbi <becem.gharbi@live.com>
2024-08-06 12:45:34 +0100

    chore(playground): revert app.config changes

--- playground/app/app.config.ts
+++ playground/app/app.config.ts
@@ -1,10 +1,19 @@
-import { generateTailwindColorThemes } from '#naive-utils'
 import { defineAppConfig } from '#imports'
 
 export default defineAppConfig({
   naiveui: {
     themeConfig: {
-      ...generateTailwindColorThemes(),
+      light: {
+        common: {
+          primaryColor: 'rgb(29, 78, 216)',
+        },
+      },
+      dark: {
+        common: {
+          bodyColor: 'rgb(16, 16, 20)',
+          primaryColor: 'rgb(96, 165, 250)',
+        },
+      },
     },
   },
 })
